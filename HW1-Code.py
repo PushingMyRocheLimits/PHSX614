a89b149bf7e53bc30ad55d01501d5d0cbefcd8ec
Levi Van Ryder <73796290+PushingMyRocheLimits@users.noreply.github.com>
2026-09-01 23:18:24 -0500

    Update HW1-Code.py

--- HW1-Code.py
+++ HW1-Code.py
@@ -66,3 +66,104 @@ print(f"<1|0> = {inner_product(ket_1, ket_0)}")
 print(f"<1|1> = {inner_product(ket_1, ket_1)}")
 # ======== CODERCISE I.1.3 =========
 # ==================================
+def measure_state(state, num_meas):
+    """Simulate a quantum measurement process.
+
+    Args:
+        state (np.array[complex]): A normalized qubit state vector.
+        num_meas (int): The number of measurements to take
+
+    Returns:
+        np.array[int]: A set of num_meas samples, 0 or 1, chosen according to the probability
+        distribution defined by the input state.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    # COMPUTE THE MEASUREMENT OUTCOME PROBABILITIES
+    prob_0 = np.abs(state[0]) ** 2
+    prob_1 = np.abs(state[1]) ** 2
+
+    # RETURN A LIST OF SAMPLE MEASUREMENT OUTCOMES
+    return np.random.choice([0, 1], size=num_meas, p=[prob_0, prob_1])
+
+    pass
+# ======== CODERCISE I.1.4 =========
+# ==================================
+U = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+
+
+def apply_u(state):
+    """Apply a quantum operation.
+
+    Args:
+        state (np.array[complex]): A normalized quantum state vector.
+
+    Returns:
+        np.array[complex]: The output state after applying U.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    # APPLY U TO THE INPUT STATE AND RETURN THE NEW STATE
+    return np.dot(U, state)
+    
+    pass
+# ======== CODERCISE I.1.5 =========
+# ==================================
+U = np.array([[1, 1], [1, -1]]) / np.sqrt(2)
+
+
+def initialize_state():
+    """Prepare a qubit in state |0>.
+
+    Returns:
+        np.array[float]: the vector representation of state |0>.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    return np.array([1.0, 0.0])
+
+    # PREPARE THE STATE |0>
+    pass
+
+
+def apply_u(state):
+    """Apply a quantum operation."""
+    return np.dot(U, state)
+
+
+def measure_state(state, num_meas):
+    """Measure a quantum state num_meas times."""
+    p_alpha = np.abs(state[0]) ** 2
+    p_beta = np.abs(state[1]) ** 2
+    meas_outcome = np.random.choice([0, 1], p=[p_alpha, p_beta], size=num_meas)
+    return meas_outcome
+
+
+def quantum_algorithm():
+    """Use the functions above to implement the quantum algorithm described above.
+
+    Try and do so using three lines of code or less!
+
+    Returns:
+        np.array[int]: the measurement results after running the algorithm 100 times
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    state = initialize_state()
+    state = apply_u(state)
+    return measure_state(state, 100)
+
+    # PREPARE THE STATE, APPLY U, THEN TAKE 100 MEASUREMENT SAMPLES
+    pass
